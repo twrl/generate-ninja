ea8ba51029854edfb7744ce4e3d30c70d3912bbd
Tom <twrl@users.noreply.github.com>
2017-04-28 12:12:27 +0100

    Update githead.py

--- build/util/githead.py
+++ build/util/githead.py
@@ -3,19 +3,24 @@ import os
 import re
 from sys import argv
 
-dirname = os.path.dirname(argv[1])
-parts = dirname.partition("/.git")
+#dirname =
+parts =  os.path.dirname(argv[1]).partition("/.git")
 if os.path.isfile(parts[0] + parts[1]):
   g = open(parts[0] + parts[1], 'r')
   gitdir = g.readline()
   g.close()
   gmatch = re.match('gitdir: (.*)', gitdir)
-  dirname = os.path.normpath(os.path.join(parts[0], gmatch.group(1)))
+  if os.path.isabs(gmatch.group(1)):
+    dirname = gmatch.group(1)
+  else:
+    dirname = os.path.normpath(os.path.join(parts[0], gmatch.group(1)))
   headfile = os.path.join(dirname, parts[2])
 else:
   headfile = argv[1]
   dirname = os.path.dirname(headfile)
 
+print "headfile: " + headfile 
+print "gitdir: " + dirname
 
 
 f = open(headfile, 'r')
