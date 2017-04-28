087021a542828a851878fe19531b8647d8a49424
Tom <twrl@users.noreply.github.com>
2017-04-28 11:36:25 +0100

    Better building as a submodule
    Modify githead.py to be aware of submodules with absorbed gitdirs

--- build/util/githead.py
+++ build/util/githead.py
@@ -4,7 +4,21 @@ import re
 from sys import argv
 
 dirname = os.path.dirname(argv[1])
-f = open(argv[1], 'r')
+parts = dirname.partition("/.git")
+if os.path.isfile(parts[0] + parts[1]):
+  g = open(parts[0] + parts[1], 'r')
+  gitdir = g.readline()
+  g.close()
+  gmatch = re.match('gitdir: (.*)', gitdir)
+  dirname = os.path.normpath(os.path.join(parts[0], gmatch.group(1)))
+  headfile = os.path.join(dirname, parts[2])
+else:
+  headfile = argv[1]
+  dirname = os.path.dirname(headfile)
+
+
+
+f = open(headfile, 'r')
 head = f.readline()
 f.close()
 match = re.match('ref: (.*)', head)
